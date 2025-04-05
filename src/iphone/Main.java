c53efbc3277b9d1d6042481acbb55cd4e0799104
Estevão <estevaog.gomes@gmail.com>
2025-04-05 15:17:38 -0300

    finishes implementation

--- src/iphone/Main.java
+++ src/iphone/Main.java
@@ -2,6 +2,18 @@ package iphone;
 
 public class Main {
 	public static void main(String[] args) {
+		IPhone iphone = new IPhone();
 		
+		System.out.println("Testando Iphone:");
+		iphone.selectMusic("Let It Go");
+		iphone.play();
+		iphone.pause();
+		iphone.answer();
+		iphone.call("(22)99298-8821");
+		iphone.callVoiceMail();
+		iphone.addNewTab();
+		iphone.showWebPage("www.apple.com");
+		iphone.refreshPage();
+		;
 	}
 }
